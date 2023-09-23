fa84ec56068e79b6d9d764a5a202a4209faf1df2
Oleh_K <oleh.usa2017@gmail.com>
2023-09-23 13:02:18 -0400

    index injection

--- src/main/java/com/cydeo/controller/Index.java
+++ src/main/java/com/cydeo/controller/Index.java
@@ -1,14 +1,24 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index {
 
+    private final AccountService accountService;
+
+    public Index(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @RequestMapping("/index")
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("accountList", accountService.listAllAccount());
+
         return "account/index";
     }
 
